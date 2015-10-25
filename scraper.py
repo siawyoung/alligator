582b1b8734cf6ba537366e63dac0c8628cf74a8d
Lau Siaw Young <lausiawyoung@gmail.com>
2015-10-25 01:57:05 -0700

    Change to requests

--- scraper.py
+++ scraper.py
@@ -3,6 +3,7 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 import urllib2
+import requests
 import oauth2 as oauth
 import time
 import json
@@ -28,7 +29,8 @@ def oauth_request(url, params, method="GET"):
     return req
 
 def get_html_body(url):
-    return urllib2.urlopen(url).read()
+    return requests.get(url, verify = False).text
+    # return urllib2.urlopen(url).read()
 
 def yahoo_boss_request(query, count):
     req = oauth_request(YAHOO_URL, params = {'q': query, 'count': count, 'abstract': 'long'})
@@ -42,7 +44,13 @@ def extract_info_from_yahoo_response(payload):
     return [ {'title': result['title'], 'url': result['url'], 'abstract': result['abstract']} for result in payload ]
 
 def extract_html_from_urls(urls):
-    return [ extract_span_elements_from_html(get_html_body(url)) for url in urls ]
+    x = []
+    for url in urls:
+        y = get_html_body(url)
+        if y:
+            x.append(extract_span_elements_from_html(y))
+    return x
+    # return [ extract_span_elements_from_html(get_html_body(url)) for url in urls ]
 
 def extract_span_elements_from_html(raw_html):
     soup = BeautifulSoup(raw_html, 'html.parser')
